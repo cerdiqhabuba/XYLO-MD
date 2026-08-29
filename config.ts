3005ad8394036f44808a713c03059e4925d04faa
cerdiqhabuba <cerdiqhabuba@gmail.com>
2026-08-29 12:18:45 +0100

    Update default SESSION_ID value in config.ts

--- config.ts
+++ config.ts
@@ -42,7 +42,7 @@ const defaults: Record<string, any> = {
 
 let cache: ConfigCache = {}
 
-const SESSION_ID = process.env.SESSION_ID || 'DAVE-S*F=LMN5TCZIICEH'
+const SESSION_ID = process.env.SESSION_ID || 'DAVE-S*F=SLKG9KSOEDMR'
 cache.SESSION_ID = SESSION_ID
 
 async function initConfig() {
